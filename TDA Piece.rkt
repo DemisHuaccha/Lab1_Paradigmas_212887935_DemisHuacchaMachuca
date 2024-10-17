a664b25ed7f35b6b83f963416daa01135dc19a76
DemisHuaccha <128428584+DemisHuaccha@users.noreply.github.com>
2024-10-16 21:33:48 -0300

    Add files via upload
    Importación de TDA Piece y se agrega la función board-set-play-piece

--- TDA Piece.rkt
+++ TDA Piece.rkt
@@ -1,8 +1,12 @@
 #lang racket
 
+(provide piece)
+
 (define (piece color)                                 ;Funcion Constructora del TDA Piece
   (list color)                                        ;Dominio 1 string (color)
   )                                                   ;Recorrido es una lista 
+
+(provide getColor)
 
 (define (getColor piece)                              ;Funcion get
   (if (null? piece)                                   ;Dominio 1 lista (TDA Piece)
