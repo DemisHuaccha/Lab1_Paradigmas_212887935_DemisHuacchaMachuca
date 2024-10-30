ff3577b0c4af80c3f59fd0472c9a0a246bdf5ea5
DemisHuaccha <128428584+DemisHuaccha@users.noreply.github.com>
2024-10-29 23:30:03 -0300

    Update TDA Game.rkt
    Se corrigen los require para corresponder a los nombres finales de los archivos

--- TDA Game.rkt
+++ TDA Game.rkt
@@ -1,9 +1,9 @@
 #lang racket
 
 
-(require "TDA-Piece.rkt")
-(require "TDA-Player.rkt")
-(require "TDA-Board.rkt")
+(require "TDA-Piece_21.288793-5_DemisHuacchaMachuca.rkt")
+(require "TDA-Player_21.288793-5_DemisHuacchaMachuca.rkt")
+(require "TDA-Board_21.288793-5_DemisHuacchaMachuca.rkt")
 
 (provide game)
 
